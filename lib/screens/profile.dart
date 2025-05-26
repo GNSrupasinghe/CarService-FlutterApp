568c8f198484049334f4ca498aeb084c6b0fc551
Pushpika Herath <138790930+mpdherath@users.noreply.github.com>
2025-05-26 10:36:37 +0000

    initalize profile screen

---
+++ lib/screens/profile.dart
@@ -0,0 +1,7 @@
+import 'dart:developer';
+
+import 'package:car_service/auth/auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
