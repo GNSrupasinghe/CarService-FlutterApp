f8986322bfd73f945edcfc7cff36893e99872076
Omindu Samarasinghe <168259063+Omindunu@users.noreply.github.com>
2025-05-26 15:31:16 +0000

    implemen enchnaced navigation system without delay

--- lib/navigation.dart
+++ lib/navigation.dart
@@ -10,3 +10,28 @@ class Navigationpages extends StatefulWidget {
   @override
   State<Navigationpages> createState() => _NavigationpagesState();
 }
+
+class _NavigationpagesState extends State<Navigationpages> {
+  int myIndex = 0;
+  List<Widget> widgetList = [Home(), History(), profile()];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: widgetList[myIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        selectedLabelStyle: GoogleFonts.jockeyOne(
+          color: Colors.orange,
+          fontSize: 20,
+        ),
+        selectedItemColor: Colors.orange,
+        unselectedLabelStyle: GoogleFonts.jockeyOne(
+          color: Colors.black,
+          fontSize: 20,
+        ),
+        unselectedItemColor: Colors.black,
+        backgroundColor: const Color.fromARGB(255, 238, 220, 156),
+        onTap: (index) {
+          setState(() {
+            myIndex = index;
+          });
+        },
