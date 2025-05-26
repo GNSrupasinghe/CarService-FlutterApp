07ea112ebb678291e4df051a0e5b86e4c082b7e8
Nadeesha Rupasinghe <136307805+GNSrupasinghe@users.noreply.github.com>
2025-05-26 11:09:10 +0000

    create structre of the splash screen

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,2 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+class Splashscreen extends StatelessWidget {
+  const Splashscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                SizedBox(height: 80),
+                Text(
+                  "CAR SERVICE",
+                  style: GoogleFonts.jockeyOne(
+                    color: Colors.amberAccent,
+                    fontSize: 60,
+                  ),
+                ),
+                Text(
+                  "SOLUTION",
+                  style: GoogleFonts.jockeyOne(
+                    color: Colors.amberAccent,
+                    fontSize: 60,
+                  ),
+                ),
+              ],
+            ),
