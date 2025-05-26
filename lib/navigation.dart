e1a55db250a21b3f034e3ece65328a9852496418
Omindu Samarasinghe <168259063+Omindunu@users.noreply.github.com>
2025-05-26 15:12:06 +0000

    import  dependencis to navigation panel

---
+++ lib/navigation.dart
@@ -0,0 +1,5 @@
+import 'package:car_service/screens/history.dart';
+import 'package:car_service/screens/home.dart';
+import 'package:car_service/screens/profile.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
