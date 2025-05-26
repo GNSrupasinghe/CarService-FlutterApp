9ded8c873718cbfdf43921358403ff4d6d298e16
Nadeesha Rupasinghe <136307805+GNSrupasinghe@users.noreply.github.com>
2025-05-26 13:03:59 +0000

    add getters and setters to the object

--- lib/detail.dart
+++ lib/detail.dart
@@ -7,4 +7,43 @@ class Detail {
   String? timeslot;
   String? mechanic;
 
+  setVehicle(String vehicle) {
+    this.vehicle = vehicle;
+  }
+
+  getVehicle() {
+    return vehicle;
+  }
+
+  settypeofservice(String typeofservice) {
+    this.typeofservice = typeofservice;
+  }
+
+  gettypeofservice() {
+    return typeofservice;
+  }
+
+  setdatetime(DateTime dateTime) {
+    this.dateTime = dateTime;
+  }
+
+  getdatetime() {
+    return dateTime;
+  }
+
+  settimeslot(String timeslot) {
+    this.timeslot = timeslot;
+  }
+
+  gettimeslot() {
+    return timeslot;
+  }
+
+  setmechanic(String mechanic) {
+    this.mechanic = mechanic;
+  }
+
+  getmechanic(String mechanic) {
+    return mechanic;
+  }
 }
