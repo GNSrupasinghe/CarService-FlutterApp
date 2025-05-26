bcf519fa5b3cc76240271ceab531e910981e7b8d
Nadeesha Rupasinghe <136307805+GNSrupasinghe@users.noreply.github.com>
2025-05-26 12:25:04 +0000

    initalize varibales to store user deatils

--- lib/loginscreen.dart
+++ lib/loginscreen.dart
@@ -2,3 +2,21 @@ import 'package:car_service/auth/auth.dart';
 import 'package:car_service/textfield.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+class Loginscreen extends StatefulWidget {
+  const Loginscreen({super.key});
+
+  @override
+  State<Loginscreen> createState() => _LoginscreenState();
+}
+
+class _LoginscreenState extends State<Loginscreen> {
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    usernameController.dispose();
+    passwordController.dispose();
+  }
