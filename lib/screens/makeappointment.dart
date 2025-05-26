a5d3aa975b2fc4c7b6cddf73560291b173248155
Shavindi977 <kawishala.shavindi@icloud.com>
2025-05-26 14:14:16 +0000

    create state under the classname

--- lib/screens/makeappointment.dart
+++ lib/screens/makeappointment.dart
@@ -2,3 +2,9 @@ import 'package:car_service/detail.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+class makeappointment extends StatefulWidget {
+  const makeappointment({super.key});
+
+  @override
+  State<makeappointment> createState() => _makeappointmentState();
+}
