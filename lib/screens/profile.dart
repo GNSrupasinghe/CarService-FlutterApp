0bc957d46cc6eb043f41070a25c566b2613958b4
Pushpika Herath <138790930+mpdherath@users.noreply.github.com>
2025-05-26 10:38:52 +0000

    get user details from firebase

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -5,3 +5,23 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+class profile extends StatelessWidget {
+  const profile({super.key});
+
+  Future<Map<String, dynamic>?> getUserData(String uid) async {
+    try {
+      final userDoc =
+          await FirebaseFirestore.instance
+              .collection('users') // Replace with your collection name
+              .doc(uid) // Use email as the document ID
+              .get();
+
+      if (userDoc.exists) {
+        return userDoc.data();
+      }
+    } catch (e) {
+      log("Error fetching user data: $e");
+    }
+    return null;
+  }
