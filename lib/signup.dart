961413042036c1dc20de45cd625fe3f2fba9c5f4
Nadeesha Rupasinghe <136307805+GNSrupasinghe@users.noreply.github.com>
2025-05-26 11:16:02 +0000

    extends from widgets

--- lib/signup.dart
+++ lib/signup.dart
@@ -2,3 +2,10 @@ import 'package:car_service/auth/auth.dart';
 import 'package:car_service/textfield.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+class signupscreen extends StatefulWidget {
+  const signupscreen({super.key});
+
+  @override
+  State<signupscreen> createState() => _signupscreenState();
+}
