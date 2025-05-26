99a79eaeadac32e41032bd1d64ea2c92ef55fe63
harshadulshan <harshakaldera540@gmail.com>
2025-05-26 07:43:11 +0000

    initalize flutter project

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:car_service/firebase_options.dart';
+import 'package:car_service/screens/booking.dart';
+import 'package:car_service/screens/confirmbooking.dart';
+import 'package:car_service/screens/contactus.dart';
+import 'package:car_service/screens/home.dart';
+import 'package:car_service/loginscreen.dart';
+import 'package:car_service/navigation.dart';
+import 'package:car_service/screens/makeappointment.dart';
+import 'package:car_service/screens/mechanic.dart';
+import 'package:car_service/screens/profile.dart';
+import 'package:car_service/screens/typeofservices.dart';
+import 'package:car_service/signup.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'splashscreen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/login': (context) => Loginscreen(),
+        '/signup': (context) => signupscreen(),
+        '/home': (context) => Home(),
+        '/nav': (context) => Navigationpages(),
+        '/booking': (context) => Booking(),
+        '/typeofservices': (context) => typeofservices(),
+        '/makeappointment': (context) => makeappointment(),
+        '/mechanic': (context) => mechanic(),
+        '/confirmbooking': (context) => confirmbooking(),
+        '/profile': (context) => profile(),
+        '/contact': (context) => contactus(),
+      },
+      home: const Splashscreen(),
+    );
+  }
+}
