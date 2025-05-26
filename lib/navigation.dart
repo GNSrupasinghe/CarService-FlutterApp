cdab469faa31732c3280b6e497e42e3737785b55
Omindu Samarasinghe <168259063+Omindunu@users.noreply.github.com>
2025-05-26 15:17:49 +0000

    add state to navigation class

--- lib/navigation.dart
+++ lib/navigation.dart
@@ -3,3 +3,10 @@ import 'package:car_service/screens/home.dart';
 import 'package:car_service/screens/profile.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+class Navigationpages extends StatefulWidget {
+  const Navigationpages({super.key});
+
+  @override
+  State<Navigationpages> createState() => _NavigationpagesState();
+}
